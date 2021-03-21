9ab8a349fd8c97cc32d2651a875baccdb50e89fc
Mert Çalışkan <mert.caliskan@bilgiyon.com.tr>
2021-03-21 23:09:10 +0300

    Menu changes

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer 
-    v-model='drawer'
-    app
-    >
+    <v-navigation-drawer v-model="drawer" app>
+      <v-img
+      class="pa-4"
+        lazy-src="https://picsum.photos/id/11/10/6"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        height="170"
+        src="https://picsum.photos/1920/1080?random"
+      >
+        <v-avatar size="100">
+          <img src="./assets/todo.jpg" alt="Todo" />
+        </v-avatar>
+
+        <div class="white--text text-subtitle-1 font-weight-bold">Vuetify Todo</div>
+        <div class="white--text text-subtitle-2">Vuetify Todo</div>
+
+      </v-img>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title">
-            Vuetify Todo
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Todo
-          </v-list-item-subtitle>
+          <v-list-item-subtitle> Menu </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
 
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.to"
-          link
-        >
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -43,6 +42,7 @@
       color="primary"
       dark
       src="https://picsum.photos/1920/1080?random"
+      height="170"
       prominent
     >
       <template v-slot:img="{ props }">
@@ -72,19 +72,20 @@
     </v-app-bar>
 
     <v-main>
-            <router-view></router-view>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data: () => ({ 
-      drawer: null,
-      items: [
-          { title: 'Dashboard', icon: 'mdi-format-list-checks', to: '/' },
-          { title: 'About', icon: 'mdi-help-box' , to:'/about'},
-        ],
-       }),
-  }
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Dashboard", icon: "mdi-format-list-checks", to: "/" },
+      { title: "About", icon: "mdi-help-box", to: "/about" },
+    ],
+  }),
+};
 </script>
+            

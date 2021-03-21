9be63fbfd57fc3048f85a09be90b937634b67074
Mert Çalışkan <mert.caliskan@bilgiyon.com.tr>
2021-03-21 22:21:03 +0300

    Edit Task Added

--- src/components/Todo/TaskMenu.vue
+++ src/components/Todo/TaskMenu.vue
@@ -21,6 +21,12 @@
       </v-list>
     </v-menu>
 
+    <dialog-edit
+      v-if="dialogs.edit"
+      @close="dialogs.edit = false"
+      :task="task"
+    />
+
     <dialog-delete
       v-if="dialogs.delete"
       @close="dialogs.delete = false"
@@ -34,6 +40,7 @@ export default {
   props: ["task"],
   data: () => ({
     dialogs: {
+      edit: false,
       delete: false,
     },
     items: [
@@ -41,7 +48,7 @@ export default {
         title: "Edit",
         icon: "mdi-pencil",
         click() {
-          console.log("Edit");
+          this.dialogs.edit = true;
         },
       },
       {
@@ -66,6 +73,7 @@ export default {
     },
   },
   components: {
+    "dialog-edit": require("@/components/Todo/Dialogs/DialogEdit.vue").default,
     "dialog-delete": require("@/components/Todo/Dialogs/DialogDelete.vue")
       .default,
   },

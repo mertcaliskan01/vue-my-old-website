4a677092629b5be627ab1ddcd0dcda672383da5a
Mert <mcaliskanmert>
2022-02-10 22:45:47 +0300

    About Page

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
   - You can access the project online from [link](https://vuetify-todo-app.herokuapp.com/#/).
   
 # Prerequisites
-  - Nodel Modules
+  - Node Modules
   - Vue.js
 
 # Project Setup
@@ -13,15 +13,15 @@ First step, we need to clone backend api repo to your local machine.
      
 # Run Project
      $ vue ui
-     $ Görevler -> serve -> click Görevi Çalıştır
+     $ Tasks -> Serve -> Run Task
 
 # Tech
-  - vue: "^2.6.11",
-  - axios: "^0.21.1"
-  - vuex: "^3.4.0"
-  - vuetify: "^2.4.0",
-  - vue-router: "^3.2.0",
-  - express: "^4.17.1",
+  - vue: 2.6.11
+  - axios: 0.21.1
+  - vuex: 3.4.0
+  - vuetify: 2.6.3
+  - vue-router: 3.2.0
+  - express: 4.17.1
   
 # About me
 Mert Çalışkan
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,87 @@
 <template>
-  <div class="about pa-16">
-    <h1>This is an about page</h1>
-    <p>
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-     A ipsam illum voluptatem saepe neque repellendus obcaecati 
-     cupiditate soluta numquam impedit eligendi iure pariatur 
-     architecto ex facilis praesentium blanditiis, cum eaque. 
-    </p>
-  </div>
+  <v-container>
+    <v-row justify="space-around">
+      <v-card width="100%">
+
+          <v-card-title class="white--text mt-8">
+            
+          </v-card-title>
+        </v-img>
+
+        <v-card-text>
+          <div class="font-weight-bold ml-8 mb-2">
+            Project
+          </div>
+
+          <v-timeline
+            align-top
+            dense
+          >
+            <v-timeline-item
+              v-for="message in messages"
+              :color="message.color"
+              small
+            >
+              <div>
+                <div class="font-weight-normal">
+                  <strong>{{ message.from }}</strong>
+                </div>
+                  <v-list-item v-for="item in message.items" :key="item.title">
+                    <v-list-item-content>
+                      <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
+
+
+<script>
+  export default {
+    data: () => ({
+      messages: [
+        {
+          from: 'Prerequisites',
+          items: [
+            { title: "Node Modules"},
+            { title: "Vue.js"},
+          ],
+          color: 'deep-purple lighten-1',
+        },
+        {
+          from: 'Project Setup',
+          items: [
+            { title: "First step, we need to clone backend api repo to your local machine."},
+            { title: "git clone https://github.com/mertcaliskan01/vuetify-todo.git"},
+          ],
+          color: 'red lighten-1',
+        },
+        {
+          from: 'Run Project',
+          items: [
+            { title: "vue ui"},
+            { title: "Tasks -> Serve -> Run task"},
+          ],
+          color: 'blue lighten-2',
+        },
+        {
+          from: 'Tech',
+          items: [
+            { title: "vue: 2.6.11"},
+            { title: "axios: 0.21.1"},
+            { title: "vuex: 3.4.0"},
+            { title: "vuetify: 2.6.3"},
+            { title: "vue-router: 3.2.0"},
+            { title: "express: 4.17.1"},
+          ],
+          color: 'green lighten-1',
+        },
+      ],
+    }),
+  }
+</script>

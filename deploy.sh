92606b18b3ea30db5c0218a1bca74211e39a38c9
Mert <mcaliskanmert>
2022-03-13 19:09:21 +0300

    deploy settings

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors  yarn deploy
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME 
+git init
+git add .
+git commit -m 'deploy'
+git push -f git@github.com:mertcaliskan01/MyWebsite.git master:gh-pages
+cd -
